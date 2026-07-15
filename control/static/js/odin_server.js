bf6e8ec9aa8f65bbe388db15c59d8516f230d63c
Sam-g-F <301502938+Sam-g-F@users.noreply.github.com>
2026-07-15 15:10:19 +0100

    Tidied UI code and removed unnecessary parts from the workshop surrounding the UI

--- control/static/js/odin_server.js
+++ control/static/js/odin_server.js
@@ -1,23 +1,14 @@
 $( document ).ready(function() {
-
-    update_api_adapters();
     poll_update()
 });
 
 function poll_update() {
-    update_background_task();
+   is_LED_test_running();
     setTimeout(poll_update, 500);   
 }
 
-function update_api_adapters() {
-
-    $.getJSON('/api/adapters/', function(response) {
-        adapter_list = Object.keys(response.adapters).join(", ")
-        $('#api-adapters').html(adapter_list);
-    });
-}
-
-function update_background_task() {
+// Presents whether the LED test is running
+function is_LED_test_running() {
 
     $.getJSON('/api/0.1/selftest/application', function(response) {
         var pattern_trigger = response.application.run_LEDs;
@@ -26,17 +17,8 @@ function update_background_task() {
     });
 }
 
-function change_enable() {
-    var enabled = $('#task-enable').prop('checked');
-    console.log("Enabled changed to " + (enabled ? "true" : "false"));
-    $.ajax({
-        type: "PUT",
-        url: '/api/workshop/background_task',
-        contentType: "application/json",
-        data: JSON.stringify({'enable': enabled})
-    });
-}
 
+// runs LED test
 function run_LED_test(){
     var LED_value = document.getElementById("run_LED_test").value;
     console.log("Running LED test")
