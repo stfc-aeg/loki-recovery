eca504427372a83eb12748542e091b9c16d18970
Sam-g-F <301502938+Sam-g-F@users.noreply.github.com>
2026-07-16 14:20:18 +0100

    Added GPIO test functionality and allowed it to be run through UI, aswell as dummy button for selecting which pins it will test

--- control/src/carrier/carrier.py
+++ control/src/carrier/carrier.py
@@ -17,15 +17,49 @@ class LokiCarrier_self_test (LokiCarrier_1v0):
         self.LED_trigger = False
         self.LED_list = ["led0", "led1", "led2", "led3", "led2", "led1", "led0" ]
 
+        self.GPIO_trigger = False
+
+        kwargs.setdefault('pin_config_id_output', 'EMIO21')
+        kwargs.setdefault('pin_config_active_low_output', False)
+        kwargs.setdefault('pin_config_is_input_output', False)
+        kwargs.setdefault('pin_config_default_value_output', 0)     # Active high so disabled by default
+
+        kwargs.setdefault('pin_config_id_input', 'EMIO22')
+        kwargs.setdefault('pin_config_is_input_input', True)
+        self.flag = False
         # MUST call the superclass init LAST
         super(LokiCarrier_self_test, self).__init__(**kwargs)
         self._logger.info('LOKI super init complete')
+        self.ADC_test()
+        
+        
 
         self.leds_enable()
+
+    def ADC_test(self):
+        if self._ltc2986.initialised:
+            with self._ltc2986.acquire(blocking=True, timeout=1) as rslt:
+                if not rslt:
+                    raise Exception('Failed to get LTC lock, timed out')
+
+                self._ltc2986.device.add_raw_adc_channel(
+                    4,
+                    False,
+                    differential=False,
+                )
+                self.flag = True
+                self._logger.info('Enabled on-LOKI-carrier ADC input')
+
+        else:
+            raise Exception('Cannot set up a channel when LTC has not been configured yet')
+
 
     # Function held in parameter tree, used to run LED pattern
     def set_LED_trigger(self, blank):
         self.LED_trigger = True
+    
+    def set_GPIO_trigger(self, blank):
+        self.GPIO_trigger = True
 
     # Function held in parameter tree, sanatises inputed list of LEDs and makes it the used list
     def set_LED_list(self, array):
@@ -41,12 +75,14 @@ class LokiCarrier_self_test (LokiCarrier_1v0):
     def _gen_app_paramtree(self):
         # This custom parameter tree function must be overridden, even if it is empty for now
         custom_pt = {
-            'run_LEDs' : (None, self.set_LED_trigger),
-            'LED_list' : (lambda: self.LED_list, self.set_LED_list)
+            'run_LEDs' : (lambda: self.LED_trigger, self.set_LED_trigger),
+            'LED_list' : (lambda: self.LED_list, self.set_LED_list),
+            'run_GPIO' : (None, self.set_GPIO_trigger )   
         }
         return custom_pt
     
     def LED_pattern(self, LED_list):
+        logging.info(self.LED_list)
         # turns all 4 LEDs off
         for LED in range(4):
            self.leds_set_led("led"+str(LED), 0)
@@ -60,6 +96,9 @@ class LokiCarrier_self_test (LokiCarrier_1v0):
     def _start_io_loops(self, options):
         super(LokiCarrier_self_test, self)._start_io_loops(options)
 
+        self.add_thread("GPIO thread", self.GPIO_loop)
+        self.watchdog_add_thread("GPIO thread", 10, lambda: logging.error("!!!! GPIO loop has failed !!!!"))
+
         self.add_thread("LEDs thread", self.LED_loop)
         self.watchdog_add_thread("LEDs thread", 30, lambda: logging.error("!!!! LED loop has failed or a pattern > 30 seconds long has been run !!!!"))
 
@@ -72,3 +111,18 @@ class LokiCarrier_self_test (LokiCarrier_1v0):
                 self.LED_trigger = False
             time.sleep(1)
 
+    def GPIO_loop(self):
+        
+        while not self.TERMINATE_THREADS:
+            self.watchdog_kick()
+            
+            if self.GPIO_trigger:
+                self.set_pin_value('output', True)
+                logging.info(self.get_pin_value('input'))
+                time.sleep(1)
+                self.set_pin_value('output', False)
+                logging.info(self.get_pin_value('input'))
+                time.sleep(1)
+                self.GPIO_trigger = False
+
+            
--- control/static/js/odin_server.js
+++ control/static/js/odin_server.js
@@ -27,15 +27,44 @@ function pull_LED_list() {
     });
 }
 
+function input_LED_list(){
+
+    var LED_list = document.getElementById("LED_list").value;
+    console.log(LED_list)
+    LED_array = LED_list.split(" ");
+    console.log(LED_array)
+
+    $.ajax({
+        type: "PUT",
+        url: '/api/0.1/selftest/application',
+        contentType: "application/json",
+        data: JSON.stringify({'LED_list':LED_array})
+    });
+}
+
 
 // runs LED test
 function run_LED_test(){
+    input_LED_list();
     var LED_value = document.getElementById("run_LED_test").value;
     console.log("Running LED test")
     $.ajax({
         type: "PUT",
-        url: '/api/0.1/selftest/application ',
+        url: '/api/0.1/selftest/application',
         contentType: "application/json",
         data: JSON.stringify({'run_LEDs': true})
+    });
+}
+
+//runs GPIO test
+function run_GPIO_test(){
+    input_LED_list();
+    var GPIO_value = document.getElementById("run_GPIO_test").value;
+    console.log("Running GPIO test")
+    $.ajax({
+        type: "PUT",
+        url: '/api/0.1/selftest/application',
+        contentType: "application/json",
+        data: JSON.stringify({'run_GPIO': true})
     });
 }
